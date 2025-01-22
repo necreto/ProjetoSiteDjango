d7916e29d7482ebd3eae3f57a1e69808fff12462
Felipe Necreto <fenecreto@gmail.com>
2025-01-22 16:39:31 -0300

    criado o template base

--- core/urls.py
+++ core/urls.py
@@ -18,7 +18,9 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
+from apps.base import views as base_views
 
+   
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
