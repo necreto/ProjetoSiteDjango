3324d1a24f4ae309b513632765ad7f76ca34d92a
Felipe Necreto <fenecreto@gmail.com>
2025-01-22 16:54:07 -0300

    criado o app pages

--- core/settings.py
+++ core/settings.py
@@ -72,6 +72,7 @@ THIRD_APPS = [ # são as Lib/app que instalamos no projeto
 
 PROJECT_APPS = [ # são os apps que criamos no projeto 
     'apps.base',		# update 11/03/2024
+    'apps.pages'
         # 'apps.myapp',   # Removido esses apps que nao criamos ainda.
 ]
 
--- core/urls.py
+++ core/urls.py
@@ -17,14 +17,15 @@ Including another URLconf
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import path
-from apps.base import views as base_views
+from django.urls import path, include
 
-   
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls), 
+    #path('base/', base_view, name='base'),
+    
+    path('', include('pages.urls')), # url do app 
 ]
 
-if settings.DEBUG: # update 03/11/2024: (em homologa com debug true adiciona rota static)
+if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
