76b86d04234517e5d59427e5de91336d1b692be2
Felipe Necreto <fenecreto@gmail.com>
2025-06-25 13:05:13 -0300

    adicionado a função de deletar uma postagem no forum

--- apps/forum/urls.py
+++ apps/forum/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
 	path('', views.lista_postagem_forum, name='lista-postagem-forum'),
  	path('criar-postagem-forum/', views.criar_postagem_forum, name='criar-postagem-forum'),
   	path('detalhe-postagem-forum/<int:id>/', views.detalhe_postagem_forum, name='detalhe-postagem-forum'),
-   path('editar-postagem-forum/<int:id>/', views.editar_postagem_forum, name='editar-postagem-forum')
+    path('editar-postagem-forum/<int:id>/', views.editar_postagem_forum, name='editar-postagem-forum'),
+    path('deletar-postagem-forum/<int:id>/', views.deletar_postagem_forum, name='deletar-postagem-forum'),
 ]
