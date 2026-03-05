334ae4b100d005749c3a0b6c8a65f28a0be72813
Felipe Necreto <fenecreto@gmail.com>
2026-03-05 10:48:33 -0300

    atualizado as aulas 43 a 47

--- apps/forum/views.py
+++ apps/forum/views.py
@@ -1,33 +1,43 @@
-from django.shortcuts import render, redirect
-from django.contrib import messages  
-from django.shortcuts import get_object_or_404
+import re
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from base.utils import add_form_errors_to_messages
-from forum.forms import PostagemForumForm
-from forum import models
 from django.http import JsonResponse
+from forum.forms import PostagemForumForm
+from django.contrib import messages  
+from forum import models
+
+
+# Lista de Postagens.
+def lista_postagem_forum(request):
+    postagens = models.PostagemForum.objects.filter(ativo=True)
+    context = {'postagens': postagens}
+    return render(request, 'lista-postagem-forum.html', context)
 
 # Lista de Postagens no Dashboard (Gerenciar)
 def lista_postagem_forum(request):
+    form_dict = {}
     if request.path == '/forum/': # Pagina forum da home, mostrar tudo ativo.
         postagens = models.PostagemForum.objects.filter(ativo=True)
         template_view = 'lista-postagem-forum.html' # lista de post da rota /forum/
     else: # Essa parte mostra no Dashboard
         user = request.user 
-        lista_grupos = ['administrador', 'colaborador']
         template_view = 'dashboard/dash-lista-postagem-forum.html' # template novo que vamos criar 
-        if any(grupo.name in lista_grupos for grupo in user.groups.all()) or user.is_superuser:
+        if ['administrador', 'colaborador'] in user.groups.all() or user.is_superuser:
             # Usuário é administrador ou colaborador, pode ver todas as postagens
             postagens = models.PostagemForum.objects.filter(ativo=True)
         else:
             # Usuário é do grupo usuário, pode ver apenas suas próprias postagens
             postagens = models.PostagemForum.objects.filter(usuario=user)
-    context = {'postagens': postagens}
-    return render(request, template_view, context)
+    
+    for el in postagens:
+        form = PostagemForumForm(instance=el) 
+        form_dict[el] = form
+        
+    context = {'postagens': postagens,'form_dict': form_dict}
+    return render(request, template_view, context) 
 
 # Cria postagens 
-@login_required
-def criar_postagem_forum(request):
+def criar_postagem_forum(request): 
     form = PostagemForumForm()
     if request.method == 'POST':
         form = PostagemForumForm(request.POST, request.FILES)
@@ -38,45 +48,68 @@ def criar_postagem_forum(request):
             # Redirecionar para uma página de sucesso ou fazer qualquer outra ação desejada
             messages.success(request, 'Seu Post foi cadastrado com sucesso!')
             return redirect('lista-postagem-forum')
-        else:
-            add_form_errors_to_messages(request, form)
-            
     return render(request, 'form-postagem-forum.html', {'form': form})
 
+# Detalhes da postagem
 def detalhe_postagem_forum(request, id):
     postagem = get_object_or_404(models.PostagemForum, id=id)
     form = PostagemForumForm(instance=postagem)
     context = {'form': form, 'postagem': postagem}
     return render(request,'detalhe-postagem-forum.html', context)
 
-# Edtar Postagem
-@login_required
-def editar_postagem_forum(request, id):
+@login_required 
+def editar_postagem_forum(request,id):
+    redirect_route = request.POST.get('redirect_route', '') 
     postagem = get_object_or_404(models.PostagemForum, id=id)
-    
-    # Verifica se o usuário autenticado é o autor da postagem
+    message = 'Seu Post '+ postagem.titulo +' foi atualizado com sucesso!' # atualizei a mensagem
     if request.user != postagem.usuario and not (
-            ['administrador', 'colaborador'] in request.user.groups.all() or request.user.is_superuser):
-            return redirect('postagem-forum-list')  # Redireciona para uma página de erro ou outra página adequada
-    
+        ['administrador', 'colaborador'] in request.user.groups.all() or request.user.is_superuser):
+        return redirect('lista-postagem-forum') # Adicionar uma rota "sem permissão"
     if request.method == 'POST':
         form = PostagemForumForm(request.POST, instance=postagem)
         if form.is_valid():
             form.save()
-            messages.warning(request, 'Seu Post '+ postagem.titulo +' \
-                foi atualizado com sucesso!')
-            return redirect('editar-postagem-forum', id=postagem.id)
-        else:
-            add_form_errors_to_messages(request, form) 
-    return JsonResponse({'status': 'Ok'}) # Coloca por enquanto.
+            messages.warning(request, message)
+            print(redirect_route)
+            return redirect(redirect_route) # Faz o redirect de acordo com a rota que estou.
+    return JsonResponse({'status': message}) # isso deixa assim por enquando. Vai que futuramente utilizaremos algo a mais.
 
-#Deletar Postagem (ID)
-@login_required
+# Deletar Postagem
+@login_required 
 def deletar_postagem_forum(request, id): 
+    redirect_route = request.POST.get('redirect_route', '') 
     postagem = get_object_or_404(models.PostagemForum, id=id)
+    message = 'Seu Post '+postagem.titulo+' foi deletado com sucesso!'
     if request.method == 'POST':
         postagem.delete()
-        messages.error(request, 'Seu Post '+ postagem.titulo +' foi deletado com sucesso!')
-        return redirect('lista-postagem-forum')
-    return render(request, 'detalhe-postagem-forum.html', {'postagem': postagem})
+        messages.error(request, message)
+        
+        if re.search(r'/forum/detalhe-postagem-forum/([^/]+)/', redirect_route): # se minha rota conter
+            return redirect('lista-postagem-forum')
+        return redirect(redirect_route)
 
+    return JsonResponse({'status':message})
+
+
+# Lista de Postagens no Dashboard (Gerenciar)
+def lista_postagem_forum(request):
+    form_dict = {}
+    if request.path == '/forum/': # Pagina forum da home, mostrar tudo ativo.
+        postagens = models.PostagemForum.objects.filter(ativo=True)
+        template_view = 'lista-postagem-forum.html' # lista de post da rota /forum/
+    else: # Essa parte mostra no Dashboard
+        user = request.user 
+        template_view = 'dashboard/dash-lista-postagem-forum.html' # template novo que vamos criar 
+        if ['administrador', 'colaborador'] in user.groups.all() or user.is_superuser:
+            # Usuário é administrador ou colaborador, pode ver todas as postagens
+            postagens = models.PostagemForum.objects.filter(ativo=True)
+        else:
+            # Usuário é do grupo usuário, pode ver apenas suas próprias postagens
+            postagens = models.PostagemForum.objects.filter(usuario=user)
+    
+    for el in postagens:
+        form = PostagemForumForm(instance=el) 
+        form_dict[el] = form
+        
+    context = {'postagens': postagens,'form_dict': form_dict}
+    return render(request, template_view, context) 
